4a187f5a17123e193f09f2e3869406443ed7f1da
dshftshr <desihafitaashri.dha@gmail.com>
2024-02-17 20:40:27 +0700

    first commit

---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: darkmagenta;
+            border: 1px solid black;
+            color: white;
+            display: flex;
+        }
+
+        .kotak h2 {
+            margin: auto;
+        }
+
+
+        .row {
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="row row-1">        
+        <div class="kotak kotak1"><h2>1</h2></div>
+    </div>
+    
+    <div class="row row-2">
+        <div class="kotak kotak2"><h2>2</h2></div>
+        <div class="kotak kotak2"><h2>2</h2></div><br>
+    </div>
+
+    <div class="row row-3">
+        <div class="kotak kotak3"><h2>3</h2></div>
+        <div class="kotak kotak3"><h2>3</h2></div>
+        <div class="kotak kotak3"><h2>3</h2></div>
+    </div>
+</body>
+</html>
