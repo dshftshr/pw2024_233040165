2f46461c37f7af172bee0551154aedc30f8e6fd9
dshftshr <desihafitaashri.dha@gmail.com>
2024-02-20 20:39:56 +0700

    mengerjakan tugas 2

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World";
+echo "<br>";
+echo "Kuliah Pemograman Web";
+echo "<br>";
+echo "123 + 10"
+echo "<br>";
+echo 3.14;
+echo "<br>";
+echo false;
+echo "<br>";
+echo "Jum'at"
+echo "<br>";
+echo '"Sandhika" kuliah hari ' . "Jum'at";
+echo "<br>";
+echo "/"Sandhika ";
+
+?>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "Desi";
+$umur = "18";
+$kampus = "Universitas Pasundan";
+echo "<p>Halo, nama saya $nama saya $umur tahun, kuliah di $kampus</p>";
